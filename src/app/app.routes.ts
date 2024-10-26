a822c2b5cfabc9e36da11275137088feb6919497
DanielCondia <daniel.condia@gmail.com>
2024-10-25 23:53:36 -0500

    :sparkles: Se agrega configuracion de router para el login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {MapComponent} from "./core/map/map.component";
+import {LoginComponent} from "./core/login/login.component";
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'mapa',
     component: MapComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
   }
 ];
---
+++ src/app/core/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  constructor(
+    private readonly router: Router
+  ) {
+  }
+
+
+  login() {
+    this.router.navigate(['/mapa']);
+  }
+}

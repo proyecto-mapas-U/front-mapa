ce5a6c01fe854ec5447ccc65f413389c1674cb2e
DanielCondia <daniel.condia@gmail.com>
2024-10-29 22:16:17 -0500

    :art: :recyle: Correccion de estilos y optimizacion de codigo

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from "@angular/router";
-import { ReactiveFormsModule, Validators,FormBuilder,FormGroup } from '@angular/forms';
-import { FormControl } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import {ReactiveFormsModule, Validators, FormBuilder, FormGroup} from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,NgIf],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  formlogin = new FormGroup({
+    'name': new FormControl('', Validators.required),
+    'numero': new FormControl('', Validators.required)
+  });
 
   constructor(
     private readonly router: Router
@@ -23,16 +28,12 @@ export class LoginComponent {
     this.router.navigate(['/mapa']);
   }
 
-get name(){
-  return this.formlogin.get('name')
-}
+  get name() {
+    return this.formlogin.get('name')
+  }
 
-get numero(){
-  return this.formlogin.get('numero')
-}
+  get numero() {
+    return this.formlogin.get('numero')
+  }
 
-  formlogin = new FormGroup({
-    'name' : new FormControl ('',Validators.required),
-    'numero' : new FormControl ('',Validators.required)
-  })
 }

25fc365c187433f15674c1e9ef05135fc6f1c157
Juan Gamboa <juangamboa589@gmail.com>
2024-10-29 20:35:15 -0500

    se implementa validadores en el login

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import { ReactiveFormsModule, Validators,FormBuilder,FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -19,4 +22,17 @@ export class LoginComponent {
   login() {
     this.router.navigate(['/mapa']);
   }
+
+get name(){
+  return this.formlogin.get('name')
+}
+
+get numero(){
+  return this.formlogin.get('numero')
+}
+
+  formlogin = new FormGroup({
+    'name' : new FormControl ('',Validators.required),
+    'numero' : new FormControl ('',Validators.required)
+  })
 }
